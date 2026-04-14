3af70855d7dfbef6a489af9d238df93379e1c078
Bruno Rombi <brunorombi@yahoo.com>
2026-04-13 21:38:44 -0300

    feat: componente principal criado

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <div className='container-md'>
+      <div className='row justify-content-center align-items-center'>
+        <div className='col-12'>
+          Hello, Investimentos
+        </div>
+        <div className='col-12'>
+
+        </div>
+        <div className='col-12'>
+
+        </div>
+      </div>
+    </div>  
+  )
+}
+
+export default App
